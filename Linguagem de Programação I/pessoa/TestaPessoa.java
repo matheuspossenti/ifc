32df60bad6c763c8aae42d457855d8278701f931
Matheus Possenti <84344863+matheuspossenti@users.noreply.github.com>
2022-10-04 07:50:11 -0300

    Add files via upload

---
+++ Linguagem de Programação I/pessoa/TestaPessoa.java
@@ -0,0 +1,11 @@
+package pessoa;
+
+public class TestaPessoa {
+	public static void main(String[] args) {
+		Pessoa p1 = new Pessoa("Thiago", "111.222.333-44", 1980);
+		
+		Pessoa p2 = new Pessoa("Luan");
+		
+		
+	}
+}
